53eb3e7b7c3d9d8ad360b8ebdc7ef6cc04bae668
user <user@example.com>
2026-07-26 15:19:19 -0300

    feat: add notification permission request for Android 13+

--- src/androidMain/kotlin/MainActivity.kt
+++ src/androidMain/kotlin/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.appcuidadoidosos
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.appcuidadoidosos.database.AppDatabase
 import com.example.appcuidadoidosos.database.DatabaseDriverFactory
@@ -10,13 +15,35 @@ import com.example.appcuidadoidosos.notifier.Notifier
 import com.example.appcuidadoidosos.viewmodel.SharedViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permissão concedida
+        } else {
+            // Permissão negada. Podemos mostrar um aviso para o usuário.
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        askNotificationPermission()
+
         val database = AppDatabase(DatabaseDriverFactory(applicationContext).createDriver())
         val notifier = Notifier(applicationContext)
         val viewModel = SharedViewModel(database, notifier, lifecycleScope)
         setContent {
             App(viewModel, notifier)
+        }
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
